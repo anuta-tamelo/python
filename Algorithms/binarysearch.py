cd7bf402296b58d4f56bac6723627cb2c705c5cf
Anna Tamelo <anna.tamelo@gmail.com>
2018-12-12 17:13:33 -0500

    adding binary search

--- Algorithms/binarysearch.py
+++ Algorithms/binarysearch.py
@@ -3,7 +3,7 @@ def binary_search(list, item):
     high = len(list) - 1
 
     while low <= high:
-        mid = (low + high)
+        mid = (low + high) // 2
         guess = list[mid]
         if guess == item:
             return mid
@@ -12,7 +12,8 @@ def binary_search(list, item):
         else:
             low = mid + 1
     return None 
-my_list = [1,3,5,7,9]
 
-print (binary_search(my_list, 3))
+print(binary_search([1, 3, 5, 7, 9], 3) == 1)
+print(binary_search([1, 3, 5, 7, 9, 19 , 20], 20) == 6)
+print(binary_search([1, 3, 5, 7, 9, 10], 1) == 0)
 
