f8efa70ebd913e2e39dd223e3564cb06a602fa51
Anna Tamelo <anna.tamelo@gmail.com>
2018-12-15 20:41:10 -0500

    adding sum recursion

---
+++ Algorithms/sum_recursion.py
@@ -0,0 +1,10 @@
+def sum(arr):
+    if len(arr) == 0:
+        return None
+    if len(arr) == 1: 
+        return arr[0]
+    else:
+        return arr[0] + sum(arr[1:])
+
+print(sum([2, 4, 6, 8, 10]))
+
